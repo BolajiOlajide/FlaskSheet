cbec250ae58796ec3474cac3c1686a8adc80af70
Bolaji Olajide <bolaji.olajide@andela.com>
2017-09-11 06:36:42 +0100

    read from google sheets

---
+++ README.md
@@ -0,0 +1,5 @@
+# FlaskSheet
+
+### Author
+
+*Bolaji Olajide *
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
+from sheets import get_credentials, read
 
 app = Flask(__name__)
+credentials = get_credentials()
 
 @app.route('/')
 def home():
-	return '<h1>Bolaji</h1>'
+	details = read(credentials)
+	return render_template('index.html', details=details)
 
 if __name__ == '__main__':
 	app.run(debug=True)
