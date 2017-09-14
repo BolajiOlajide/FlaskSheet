cea67a76c7a68222734c11df454ef20331d783df
Bolaji Olajide <bolaji.olajide@andela.com>
2017-09-14 22:40:26 +0100

    add checkbox for status

--- forms.py
+++ forms.py
@@ -3,6 +3,6 @@ from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 class AddTodoForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    name = StringField('task', validators=[DataRequired()])
     description = StringField('description', validators=[DataRequired()])
-    status = StringField('status', default=False, validators=[DataRequired()])
+    status = BooleanField()
